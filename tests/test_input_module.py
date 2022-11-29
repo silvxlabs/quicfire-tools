34788a99922f6824f8d017cf673b8fdc6884d7e7
Anthony Marcozzi <anthonymarcozzi@gmail.com>
2022-11-29 16:35:52 -0700

    Add ignition and fuel tests

--- tests/test_input_module.py
+++ tests/test_input_module.py
@@ -5,6 +5,9 @@ import sys
 sys.path.append("../quicfire_tools")
 from quicfire_tools.inputs import InputModule
 
+# External Imports
+import pytest
+
 TEST_PARAMS = {
     "nx": 100,
     "ny": 100,
@@ -23,6 +26,66 @@ TEST_PARAMS = {
 }
 
 SUT = InputModule("pytest-simulation")
+
+
+def test_write_fuel_data():
+    # Test fuel flag 1
+    fuel_data_params = TEST_PARAMS.copy()
+    fuel_data_params["fuel_flag"] = 1
+    fuel_data_params["fuel_density"] = 1.25
+    fuel_data_params["fuel_moisture"] = 0.75
+    fuel_data_params["fuel_height"] = 0.5
+    fuel_params = SUT._write_fuel(fuel_data_params)
+    assert fuel_params == ("\n1.25", "\n0.75", "\n0.5")
+
+    # Test fuel flag 1
+    fuel_data_params = TEST_PARAMS.copy()
+    fuel_data_params["fuel_flag"] = 1
+    fuel_data_params["fuel_density"] = 0.0
+    fuel_data_params["fuel_moisture"] = 0.0
+    fuel_data_params["fuel_height"] = 0.0
+    fuel_params = SUT._write_fuel(fuel_data_params)
+    assert fuel_params == ("\n0.0", "\n0.0", "\n0.0")
+
+    # Test fuel flag 1
+    fuel_data_params = TEST_PARAMS.copy()
+    fuel_data_params["fuel_flag"] = 1
+    with pytest.raises(KeyError):
+        fuel_params = SUT._write_fuel(fuel_data_params)
+
+    # Test fuel flag 3
+    fuel_data_params = TEST_PARAMS.copy()
+    fuel_data_params["fuel_flag"] = 3
+    fuel_params = SUT._write_fuel(fuel_data_params)
+    assert fuel_params == ("", "", "")
+
+    # Test fuel flag 3 with fuel data
+    fuel_data_params = TEST_PARAMS.copy()
+    fuel_data_params["fuel_flag"] = 3
+    fuel_data_params["fuel_density"] = 1.25
+    fuel_data_params["fuel_moisture"] = 0.75
+    fuel_data_params["fuel_height"] = 0.5
+    fuel_params = SUT._write_fuel(fuel_data_params)
+    assert fuel_params == ("", "", "")
+
+    # Test fuel flag 4
+    fuel_data_params = TEST_PARAMS.copy()
+    fuel_data_params["fuel_flag"] = 4
+    fuel_params = SUT._write_fuel(fuel_data_params)
+    assert fuel_params == ("", "", "")
+
+
+def test_write_ignition_locations():
+    # Test custom ignition flag 6
+    ignition_custom_params = TEST_PARAMS.copy()
+    ignition_custom_params["ignition_flag"] = 6
+    assert SUT._write_ignition_locations(ignition_custom_params) == ""
+
+    # Test invalid ignition flag
+    ignition_custom_params = TEST_PARAMS.copy()
+    ignition_custom_params["ignition_flag"] = 0
+    with pytest.raises(ValueError):
+        SUT._write_ignition_locations(ignition_custom_params)
 
 
 def test_write_line_fire_ignition():
@@ -45,12 +108,32 @@ def test_write_line_fire_ignition():
 
     # Test south wind
     ignition_line_params["wind_direction"] = 180
-    assert SUT._write_line_fire_ignition(ignition_line_params) == east_wind
+    assert SUT._write_line_fire_ignition(ignition_line_params) == south_wind
 
     # Test west wind
     ignition_line_params["wind_direction"] = 270
     assert SUT._write_line_fire_ignition(ignition_line_params) == west_wind
 
-    # Test invalid wind direction
-    ignition_line_params["wind_direction"] = "invalid"
+    # Test 45 degree wind
+    ignition_line_params["wind_direction"] = 45
+    assert SUT._write_line_fire_ignition(ignition_line_params) == north_wind
 
+    # Test 45.01 degree wind
+    ignition_line_params["wind_direction"] = 45.01
+    assert SUT._write_line_fire_ignition(ignition_line_params) == east_wind
+
+    # Test -45 degree wind
+    ignition_line_params["wind_direction"] = -45
+    assert SUT._write_line_fire_ignition(ignition_line_params) == north_wind
+
+    # Test -45.01 degree wind
+    ignition_line_params["wind_direction"] = -45.01
+    assert SUT._write_line_fire_ignition(ignition_line_params) == west_wind
+
+    # Test 134.99 degree wind
+    ignition_line_params["wind_direction"] = 134.99
+    assert SUT._write_line_fire_ignition(ignition_line_params) == east_wind
+
+    # Test 135 degree wind
+    ignition_line_params["wind_direction"] = 135
+    assert SUT._write_line_fire_ignition(ignition_line_params) == south_wind
