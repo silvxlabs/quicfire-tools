ffb8954db3847763c3964d8a9a62a798012068ca
amarcozzi <anthonymarcozzi@gmail.com>
2023-07-13 14:45:41 -0600

    Define initial namespace.

---
+++ quicfire_tools/__init__.py
@@ -0,0 +1,6 @@
+from quicfire_tools.inputs import InputModule, SimulationParameters
+from quicfire_tools.outputs import SimulationOutputs
+
+__all__ = ['InputModule',
+           'SimulationParameters',
+           'SimulationOutputs']
