aa042bf61e3973aef7d3b600fbccd95207b82180
amarcozzi <anthonymarcozzi@gmail.com>
2023-07-17 13:39:54 -0600

    Add methods to write output files to numpy and zarr arrays

--- quicfire_tools/outputs.py
+++ quicfire_tools/outputs.py
@@ -6,14 +6,18 @@ from __future__ import annotations
 # Core imports
 from pathlib import Path
 
+# Internal imports
+from quicfire_tools.parameters import SimulationParameters
+
 # External imports
+import zarr
 import numpy as np
 from numpy import ndarray
 
 FUELS_OUTPUTS = {
     "fire-energy_to_atmos": {
         "format": "gridded",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
@@ -23,7 +27,7 @@ FUELS_OUTPUTS = {
     },
     "fire-reaction_rate": {
         "format": "compressed",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
@@ -32,7 +36,7 @@ FUELS_OUTPUTS = {
     },
     "fuels-dens": {
         "format": "compressed",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
@@ -41,7 +45,7 @@ FUELS_OUTPUTS = {
     },
     "fuels-moist": {
         "format": "compressed",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
@@ -50,28 +54,28 @@ FUELS_OUTPUTS = {
     },
     "groundfuelheight": {
         "format": "gridded",
-        "dimensions": ["x", "y"],
+        "dimensions": 2,
         "grid": "fire",
         "delimiter": None,
         "extension": ".bin",
-        "description": "2D array with initial fuel height in the ground layer.",
-        "units": "m"
+        "description": "2D array with initial fuel height in the ground layer",
+        "units": "m",
     },
     "mburnt_integ": {
         "format": "gridded",
-        "dimensions": ["x", "y"],
+        "dimensions": 2,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
-        "description": "2D file containing the % of mass burnt for each (i,j) "
+        "description": "2D file containing the percentage of mass burnt for each (i,j) "
                        "location on the fire grid (vertically integrated)",
         "units": "%",
-    }
+    },
 }
 THERMAL_RADIATION_OUTPUTS = {
     "thermaldose": {
         "format": "compressed",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
@@ -81,7 +85,7 @@ THERMAL_RADIATION_OUTPUTS = {
     },
     "thermalradiation": {
         "format": "compressed",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
@@ -93,7 +97,7 @@ THERMAL_RADIATION_OUTPUTS = {
 WIND_OUTPUTS = {
     "windu": {
         "format": "gridded",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "",
         "extension": ".bin",
@@ -102,7 +106,7 @@ WIND_OUTPUTS = {
     },
     "windv": {
         "format": "gridded",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "",
         "extension": ".bin",
@@ -111,7 +115,7 @@ WIND_OUTPUTS = {
     },
     "windw": {
         "format": "gridded",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "",
         "extension": ".bin",
@@ -122,21 +126,21 @@ WIND_OUTPUTS = {
 EMISSIONS_OUTPUTS = {
     "co-emissions": {
         "format": "compressed",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
-        "description": "Mass of CO emitted between two emission file output "
+        "description": "Mass of CO emitted between two emission file output"
                        "times in grams",
         "units": "g",
     },
     "pm-emissions": {
         "format": "compressed",
-        "dimensions": ["x", "y", "z"],
+        "dimensions": 3,
         "grid": "fire",
         "delimiter": "-",
         "extension": ".bin",
-        "description": "Mass of PM2.5 emitted between two emission file output "
+        "description": "Mass of PM2.5 emitted between two emission file output"
                        "times in grams",
         "units": "g",
     },
@@ -151,15 +155,17 @@ OUTPUTS_MAP = {
 
 class SimulationOutputs:
 
-    def __init__(self, outputs_directory: Path | str) -> None:
+    def __init__(self, outputs_directory: Path | str,
+                 params: SimulationParameters) -> None:
         # Convert to Path and resolve
         outputs_directory = Path(outputs_directory).resolve()
 
-        # Validate
+        # Validate outputs directory
         self._validate_outputs_dir(outputs_directory)
 
-        # Assign
+        # Assign attributes
         self._outputs_directory = outputs_directory
+        self._params = params
 
         # Build a list of present output files and their times
         self.simulation_output_files = self._build_output_files_map()
@@ -167,35 +173,60 @@ class SimulationOutputs:
         # Get indexing information from the fire grid
         self._fire_indexes = self._process_fire_indexes()
 
-    def _validate_outputs_dir(self, outputs_directory):
+        # Build a map of output formats to functions that process them
+        self._format_to_func = {
+            'compressed': self._process_compressed_bin,
+            'gridded': self._process_gridded_bin
+        }
+
+    def get_available_outputs(self) -> list[str]:
+        """Get a list of available output variables."""
+        return list(self.simulation_output_files.keys())
+
+    def get_output_times(self, output):
+        """Get list of output times for a given output."""
+
+        # Validate output
+        if output not in self.simulation_output_files:
+            raise ValueError(f"{output} is not a valid output")
+
+        # Return saved times for that output
+        return self.simulation_output_files[output]
+
+    @staticmethod
+    def _validate_outputs_dir(outputs_directory):
         """Validate the outputs directory."""
         # Check if the outputs directory exists and is a directory
         if not outputs_directory.exists():
             raise FileNotFoundError(
-                f"The directory {outputs_directory} does not exist.")
+                f"The directory {outputs_directory} does not exist."
+            )
         elif not outputs_directory.is_dir():
             raise NotADirectoryError(
-                f"The path {outputs_directory} is not a directory.")
-        
+                f"The path {outputs_directory} is not a directory."
+            )
+
         # Check for required files
-        required_files = ['fire_indexes.bin'] 
+        required_files = ["fire_indexes.bin"]
         for f in required_files:
             if not (outputs_directory / f).exists():
-                raise FileNotFoundError(f"Required file {f} not found in outputs directory")
+                raise FileNotFoundError(
+                    f"Required file {f} not found in outputs directory"
+                )
 
-    def _build_output_files_map(self):
+    def _build_output_files_map(self) -> dict[str, list[int]]:
         """Build a dictionary of present output files and their times."""
         outputs_dict = {}
         for output in OUTPUTS_MAP.keys():
-            output_paths = self._get_output_paths(output)
+            output_paths = self._get_list_output_paths(output)
 
             # Skip if no output files are found
             if not output_paths:
                 continue
 
             # Check if the output is a single file
-            if OUTPUTS_MAP[output]['delimiter'] is None:
-                # Output files with no delimiter are single files w/o time steps
+            if OUTPUTS_MAP[output]["delimiter"] is None:
+                # Outputs with no delimiter are single files w/o time steps
                 outputs_dict[output] = [0]
                 continue
 
@@ -203,12 +234,14 @@ class SimulationOutputs:
             outputs_dict[output] = self._get_output_times(output, output_paths)
         return outputs_dict
 
-    def _get_output_paths(self, output):
-        """Get a list of output files for the given output."""
-        return list(self._outputs_directory.glob(f"{output}*"))
+    def _get_list_output_paths(self, output) -> list[Path]:
+        """Get a sorted list of output files for the given output."""
+        paths = list(self._outputs_directory.glob(f"{output}*"))
+        paths.sort()
+        return paths
 
-    def _get_output_times(self, output, output_paths):
-        """Get a list of output times for the given output."""
+    def _get_output_times(self, output, output_paths) -> list[int]:
+        """Get a sorted list of output times for the given output."""
         output_times = []
         for output_path in output_paths:
             output_time = self._get_output_file_time(output, output_path)
@@ -217,12 +250,18 @@ class SimulationOutputs:
         return output_times
 
     @staticmethod
-    def _get_output_file_time(output, fpath):
+    def _get_output_file_time(output, fpath) -> int:
         """Get the time of the output file."""
         delimiter = OUTPUTS_MAP[output]['delimiter']
         extension = OUTPUTS_MAP[output]['extension']
         file_time = fpath.name.split(delimiter)[-1].split(extension)[0]
         return int(file_time)
+
+    def get_output_file_path(self, output_name, time):
+        padded_time = str(time).zfill(5)
+        file_name = f"{output_name}{OUTPUTS_MAP[output_name]['delimiter']}" \
+                    f"{padded_time}{OUTPUTS_MAP[output_name]['extension']}"
+        return self._outputs_directory / file_name
 
     def _process_fire_indexes(self) -> ndarray:
         """
@@ -239,7 +278,7 @@ class SimulationOutputs:
         Function returns a 2D ndarray of shape (num_cells, 3) representing the
         i, j, k indexes of each cell.
         """
-        with open(self._outputs_directory / 'fire_indexes.bin', 'rb') as fid:
+        with open(self._outputs_directory / "fire_indexes.bin", "rb") as fid:
             # Read in the number of cells and skip the header
             num_cells = np.fromfile(fid, dtype=np.int32, offset=4, count=1)[0]
 
@@ -273,15 +312,19 @@ class SimulationOutputs:
         # Initialize the 3D array
         full_3d_array = np.zeros(dim_yxz, dtype=np.float32)
 
-        with open(filename, 'rb') as fid:
+        with open(filename, "rb") as fid:
             # Read in the sparse values
-            sparse_values = np.fromfile(fid, dtype=np.float32, offset=4,
-                                        count=self._fire_indexes.shape[0])
+            sparse_values = np.fromfile(
+                fid, dtype=np.float32, offset=4,
+                count=self._fire_indexes.shape[0]
+            )
 
             # Map indices of the sparse data to the indices of the dense array
-            indices = (self._fire_indexes[:, 1],
-                       self._fire_indexes[:, 0],
-                       self._fire_indexes[:, 2])
+            indices = (
+                self._fire_indexes[:, 1],
+                self._fire_indexes[:, 0],
+                self._fire_indexes[:, 2],
+            )
 
             # Update the full array with the sparse indices
             full_3d_array[indices] = sparse_values
@@ -300,7 +343,7 @@ class SimulationOutputs:
         # Initialize the array
         array = np.zeros(dims, dtype=np.float32)
 
-        with open(filename, 'rb') as f:
+        with open(filename, "rb") as f:
             # Read header
             _ = np.fromfile(f, dtype=np.float32, count=1)
 
@@ -323,4 +366,107 @@ class SimulationOutputs:
         """
         # Read in gridded data
         plane = np.fromfile(fid, dtype=np.float32, count=ny * nx)
-        return np.reshape(plane, (ny, nx), order='F')
+        return np.reshape(plane, (ny, nx), order="F")
+
+    def to_zarr(self, fpath: Path):
+        """Write the data to a zarr file."""
+        # Create the zarr file
+        zarr_file = zarr.open(str(fpath), mode="w")
+
+        # Write the data to the zarr file
+        for output_name, output_times in self.simulation_output_files.items():
+            num_time_steps = len(output_times)
+            num_dimensions = OUTPUTS_MAP[output_name]['dimensions']
+            if num_dimensions == 2:
+                dims = (num_time_steps, self._params.ny, self._params.nx)
+            elif num_dimensions == 3:
+                dims = (num_time_steps, self._params.ny, self._params.nx,
+                        self._params.nz)
+            else:
+                raise ValueError(f"Unknown number of dimensions: "
+                                 f"{num_dimensions}")
+            chunk_size = [1 if i == 0 else dims[i] for i in range(len(dims))]
+            zarr_file.create_dataset(
+                output_name,
+                shape=dims,
+                chunks=chunk_size,
+                dtype=float)
+            for time_step in range(len(output_times)):
+                data = self.to_numpy(output_name, time_step)
+                zarr_file[output_name][time_step, ...] = data
+
+        return zarr_file
+
+    def to_numpy(self, output: str,
+                 timestep: None | int | list[int] = None) -> np.ndarray:
+        """Return a numpy array for the given output and timestep(s)."""
+        # Validate inputs
+        self._validate_output(output)
+        self._validate_timesteps(timestep, output)
+
+        # Get processing function for the output format
+        output_format = OUTPUTS_MAP[output]['format']
+        function = self._format_to_func.get(output_format)
+        if function is None:
+            raise ValueError(f"Unknown output format: {output_format}")
+
+        # Select files to process based on timestep input
+        output_files = self._get_list_output_paths(output)
+        if timestep is None:
+            selected_files = output_files
+        elif isinstance(timestep, int):
+            selected_files = [output_files[timestep]]
+        else:
+            selected_files = [output_files[ts] for ts in timestep]
+
+        # Process the data for the selected files
+        data = self._get_multiple_timesteps(output, selected_files, function)
+
+        return data
+
+    def _validate_output(self, output: str):
+        """Validate output."""
+        if output not in self.get_available_outputs():
+            raise ValueError(f"{output} is not a valid output. Valid outputs "
+                             f"are:  {self.get_available_outputs()}")
+
+    def _validate_timesteps(self, timestep: int | list[int] | None,
+                            output: str):
+        """Validate timestep(s)."""
+        num_timesteps = len(self.get_output_times(output))
+        if timestep is not None:
+            if isinstance(timestep, int):
+                timestep = [timestep]  # Convert to list
+            invalid_timesteps = [ts for ts in timestep if
+                                 not 0 <= ts < num_timesteps]
+            if invalid_timesteps:
+                raise ValueError(
+                    f"The following timestep(s) are not valid for this output: "
+                    f"{invalid_timesteps}. Valid timesteps are in the range "
+                    f"[0, {num_timesteps - 1}].")
+
+    def _get_single_timestep(self,
+                             output_name: str,
+                             output_file: Path,
+                             func: callable) -> np.ndarray:
+        """Return a numpy array for the given output file."""
+        dimensions = OUTPUTS_MAP[output_name]['dimensions']
+        if dimensions == 2:
+            dims = (self._params.ny, self._params.nx)
+        elif dimensions == 3:
+            dims = (self._params.ny, self._params.nx, self._params.nz)
+        else:
+            raise ValueError(f"Unknown dimensions: {dimensions}")
+        return func(output_file, dims)
+
+    def _get_multiple_timesteps(self, output_name: str,
+                                output_files: list[Path],
+                                func: callable) -> np.ndarray:
+        """Return a numpy array for the given output files."""
+        arrays = []
+        for output_file in output_files:
+            data = self._get_single_timestep(output_name, output_file, func)
+            arrays.append(data)
+        if len(arrays) == 1:
+            return arrays[0]
+        return np.stack(arrays)
