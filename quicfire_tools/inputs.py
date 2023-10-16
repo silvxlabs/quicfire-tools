c2ad7d60586c275f85335d03a67de50d8c2e9abf
ntutland <56928897+ntutland@users.noreply.github.com>
2023-10-16 10:43:59 -0400

    Create ignitions.py

--- quicfire_tools/inputs.py
+++ quicfire_tools/inputs.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 
 # Internal Imports
 from quicfire_tools.utils import compute_parabolic_stretched_grid
+from quicfire_tools.ignitions import (IgnitionType, RectangleIgnition, SquareRingIgnition, 
+                                      CircularRingIgnition)
 
 # Core Imports
 import json
@@ -13,7 +15,6 @@ import importlib.resources
 from pathlib import Path
 from typing import Literal
 from string import Template
-from enum import Enum
 
 # External Imports
 import numpy as np
@@ -115,87 +116,6 @@ class InputFile(BaseModel, validate_assignment=True):
     @classmethod
     def from_dict(cls, data: dict):
         return cls(**data)
-
-class IgnitionSources(Enum):
-    rectangle = 1
-    square_ring = 2
-    circular_ring = 3
-    ignite_dat_file = 6
-
-
-class IgnitionType(BaseModel):
-    ignition_flag: IgnitionSources
-
-    def __str__(self):
-        return (f"{self.ignition_flag.value}\t! 1 = rectangle, "
-                f"2 = square ring, 3 = circular ring, "
-                f"4 = file (QF_Ignitions.inp), "
-                f"5 = time-dependent ignitions (QF_IgnitionPattern.inp), "
-                f"6 = ignite.dat (firetech)")
-
-
-class RectangleIgnition(IgnitionType):
-    ignition_flag: IgnitionSources = 1
-    x_min: float
-    y_min: float
-    x_length: float
-    y_length: float
-
-    def __str__(self):
-        flag_line = super().__str__()
-        locations = (f"{self.x_min}\t! South-west corner in the x-direction\n"
-                     f"{self.y_min}\t! South-west corner in the y-direction\n"
-                     f"{self.x_length}\t! Length in the x-direction\n"
-                     f"{self.y_length}\t! Length in the y-direction")
-        return flag_line + locations
-
-
-class SquareRingIgnition(IgnitionType):
-    ignition_flag: IgnitionSources = 2
-    x_min: float
-    y_min: float
-    x_length: float
-    y_length: float
-    x_width: float
-    y_width: float
-
-    def __str__(self):
-        flag_line = super().__str__()
-        locations = (f"{self.x_min}\t! South-west corner in the x-direction\n"
-                     f"{self.y_min}\t! South-west corner in the y-direction\n"
-                     f"{self.x_length}\t! Length in the x-direction\n"
-                     f"{self.y_length}\t! Length in the y-direction\n"
-                     f"{self.x_width}\t! Width in the x-direction\n"
-                     f"{self.y_width}\t! Width in the y-direction")
-        return flag_line + locations
-
-
-class CircularRingIgnition(IgnitionType):
-    ignition_flag: IgnitionSources = 3
-    x_min: float
-    y_min: float
-    x_length: float
-    y_length: float
-    ring_width: float
-
-    def __str__(self):
-        flag_line = super().__str__()
-        locations = (f"{self.x_min}\t! South-west corner in the x-direction\n"
-                     f"{self.y_min}\t! South-west corner in the y-direction\n"
-                     f"{self.x_length}\t! Length in the x-direction\n"
-                     f"{self.y_length}\t! Length in the y-direction\n"
-                     f"{self.ring_width}\t! Width of the ring")
-        return flag_line + locations
-
-
-class IgniteDatFile(IgnitionType):
-    ignition_flag: IgnitionSources = 6
-    filename: str
-
-    def __str__(self):
-        flag_line = super().__str__()
-        return flag_line
-
 
 class OutputTimes(BaseModel):
     out_time_fire: PositiveInt
