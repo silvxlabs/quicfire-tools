f6e3db5f83d83e730c186b552c1f8219148ff2a9
ntutland <56928897+ntutland@users.noreply.github.com>
2023-10-17 12:09:35 -0400

    Add QU_movingcoords class

--- quicfire_tools/inputs.py
+++ quicfire_tools/inputs.py
@@ -968,3 +968,24 @@ class QFire_Plume_Advanced_User_Inputs(InputFile):
             max_points_along_plume_edge=int(lines[14].split()[0]),
             plume_to_grid_intersection_flag=int(lines[15].split()[0]),
         )
+
+class QU_movingcoords(InputFile):
+    """
+    Class representing the QU_movingcoords.inp input file.
+    This is a QUIC legacy file that is not modified for QUIC-Fire use.
+    """
+    name: str = "QU_movingcoords"
+    _extension: str = ".inp"
+
+    @classmethod
+    def from_file(cls, directory: str | Path):
+        if isinstance(directory, str):
+            directory = Path(directory)
+
+        with open(directory / "QU_movingcoords.inp", "r") as f:
+            lines = f.readlines()
+        
+        if int(lines[1].strip().split("!")[0]) == 1:
+            print("WARNING: QU_movingcoords.inp: Moving coordinates flag == 1 not supported.")
+        
+        return cls
